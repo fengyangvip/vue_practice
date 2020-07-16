bdebfcf49a4867e760f834fcd66c832e21ecd3f3
fengyangvip <fengyangvip@qq.com>
2020-07-16 23:12:49 +0800

    底部tabbar完成

---
+++ src/views/home/Home.vue
@@ -0,0 +1,50 @@
+<template>
+ <div>shouye</div>
+</template>
+
+<script>
+
+
+
+</script>
+
+<style scoped>
+  #home {
+    /*padding-top: 44px;*/
+    height: 100vh;
+    position: relative;
+  }
+
+  .home-nav {
+    background-color: var(--color-tint);
+    color: #fff;
+
+    position: fixed;
+    left: 0;
+    right: 0;
+    top: 0;
+    z-index: 9;
+  }
+
+  .tab-control {
+    position: sticky;
+    top: 44px;
+    z-index: 9;
+  }
+
+  .content {
+    overflow: hidden;
+
+    position: absolute;
+    top: 44px;
+    bottom: 49px;
+    left: 0;
+    right: 0;
+  }
+
+  /*.content {*/
+    /*height: calc(100% - 93px);*/
+    /*overflow: hidden;*/
+    /*margin-top: 44px;*/
+  /*}*/
+</style>
