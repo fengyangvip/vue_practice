d58432e303627177c7797a6aa94d7ec4ac3d600b
你们的阳哥 <fengyangvip@qq.com>
2020-07-17 17:07:24 +0800

    axios封装

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,10 +1,23 @@
 <template>
- <div>shouye</div>
+ <div id="home">
+  <nav-bar class="home-nav">
+     <div slot="center">购物街</div>
+  </nav-bar>
+ </div>
 </template>
 
 <script>
+import NavBar from 'components/common/navbar/NavBar'
+import {getHomeMultidata} from 'network/home'
+export default {
+    name:'Home',
+    components:{
+        NavBar
+    },
+    created(){
 
-
+    }
+}
 
 </script>
 
